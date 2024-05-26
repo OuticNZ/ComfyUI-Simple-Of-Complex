5b930654c5adeb5b3f4c3527eb05b4f11c013dcb
OuticNZ <roland.bird@gmail.com>
2024-05-27 08:11:59 +1200

    update: add prompt node class mapping

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .nodes.nodes import *
 
 NODE_CLASS_MAPPINGS = { 
     "Text Switch 2 Way": TextSwitch2Way
+    "Prompt Tidy": PromptTidy
     }
     
 print("\033[34mComfyUI Simple of Complex Nodes: \033[92mLoaded\033[0m")
