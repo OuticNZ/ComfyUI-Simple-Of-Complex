c992f6796846d8403179b26e911fbb97d4eb632f
OuticNZ <roland.bird@gmail.com>
2024-06-12 05:44:02 +1200

    disable pipe

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from .nodes.nodes import *
-from .nodes.pipe import *
+#from .nodes.pipe import *
 
 NODE_CLASS_MAPPINGS = { 
     "Text Switch 2 Way": TextSwitch2Way,
     "Prompt Tidy": PromptTidy,
-    "Pipe To Parameters": SoCParameters2Pipe,
-    "Pipe From Parameters": SoCPipe2Parameters,
+    #"Pipe To Parameters": SoCParameters2Pipe,
+    #"Pipe From Parameters": SoCPipe2Parameters,
     }
     
 print("\033[34mComfyUI Simple of Complex Nodes: \033[92mLoaded\033[0m")
